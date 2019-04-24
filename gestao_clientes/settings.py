fb821bdc59a3495aeae6ccedeebbc9852e6a99da
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-04-24 16:23:28 -0300

    Alteração com Bootstrap Aula 06 - Corrigindo o Settings

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
-    'https://gestao-clientes-ollyver.herokuapp.com/',
+    'gestao-clientes-ollyver.herokuapp.com',
     '127.0.0.1'
 ]
 
