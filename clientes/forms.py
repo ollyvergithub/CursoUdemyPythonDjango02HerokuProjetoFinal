a21ff87704c387306bdff7087b33f4a4bf905b0a
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-04-25 10:19:12 -0300

    Personalizando os campos do Formulário def __init__

--- clientes/forms.py
+++ clientes/forms.py
@@ -1,9 +1,21 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Person
 
-class PersonForm(ModelForm):
+class PersonForm(forms.ModelForm):
+    '''
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['photo'].widget.attrs.update({'class': 'btn btn-primary'})
+    '''
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['photo'].widget.attrs.update({'class': 'btn btn-primary'})
+
     class Meta:
         model = Person
+
         fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo' ]
         labels= {
             'first_name': 'Nome',
@@ -13,3 +25,8 @@ class PersonForm(ModelForm):
             'bio': 'Biografia',
             'photo': 'Imagem',
         }
+
+
+
+
+
