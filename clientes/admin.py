4570ef7623f66edfab3787f88db24e5795247a35
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-05-09 08:58:40 -0300

    Colocando arquivos estáticos na Amazon

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Person
+from .models import Documento
+from .models import Venda
+from .models import Produto
 
 admin.site.register(Person)
+admin.site.register(Documento)
+admin.site.register(Venda)
+admin.site.register(Produto)
 
 # Register your models here.
--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -31,10 +31,14 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+'''
 ALLOWED_HOSTS = [
     'gestao-clientes-ollyver.herokuapp.com',
     '127.0.0.1'
 ]
+'''
+
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
@@ -135,7 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# Comentei para a Amazon
+#STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     'estaticos',
@@ -154,3 +159,20 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_URL = '/login/'
 
 LOGIN_REDIRECT_URL = '/clientes/list/'
+
+# Configurações Amazon
+# Os dados config('AWS_ACCESS_KEY_ID') vem do arquivo .env
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'curso-django-heroku-final'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
